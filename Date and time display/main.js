8584e635bd2f340e3e313d51b8aa60599f9cf0b5
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-10-16 14:04:58 +0530

    adding js and css

--- Date and time display/main.js
+++ Date and time display/main.js
@@ -1,78 +1,30 @@
-const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-  const weekdays = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-  ];
-  const giveaway = document.querySelector('.giveaway');
-  const deadline = document.querySelector('.deadline');
-  const items = document.querySelectorAll('.deadline-format h4');
-  
-  let tempDate = new Date();
-  let tempYear = tempDate.getFullYear();
-  let tempMonth = tempDate.getMonth();
-  let tempDay = tempDate.getDate();
-  const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
-  
-  const year = futureDate.getFullYear();
-  const hours = futureDate.getHours();
-  const minutes = futureDate.getMinutes();
-  
-  let month = futureDate.getMonth();
-  month = months[month];
-  const weekday = weekdays[futureDate.getDay()];
-  const date = futureDate.getDate();
-  giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
-  
-  const futureTime = futureDate.getTime();
-  function getRemaindingTime() {
-    const today = new Date().getTime();
-  
-    const t = futureTime - today;
-    const oneDay = 24 * 60 * 60 * 1000;
-    const oneHour = 60 * 60 * 1000;
-    const oneMinute = 60 * 1000;
-    let days = t / oneDay;
-    days = Math.floor(days);
-    let hours = Math.floor((t % oneDay) / oneHour);
-    let minutes = Math.floor((t % oneHour) / oneMinute);
-    let seconds = Math.floor((t % oneMinute) / 1000);
-  
-    const values = [days, hours, minutes, seconds];
-    function format(item) {
-      if (item < 10) {
-        return (item = `0${item}`);
-      }
-      return item;
+function showTime(){
+    var date = new Date()
+    var h = date.getHours()
+    var m = date.getMinutes()
+    var s = date.getSeconds()
+    var session = "AM"
+
+    if (h=0){
+        h=12;
     }
-  
-    items.forEach(function (item, index) {
-      item.innerHTML = format(values[index]);
-    });
-  
-    if (t < 0) {
-      clearInterval(countdown);
-      deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM"
     }
-  }
-  let countdown = setInterval(getRemaindingTime, 1000);
-  
-  getRemaindingTime();
-  
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+   var time = h + ":" + m + ":" + s + session
+
+  document.getElementById("MyClockDisplay").innerHTML = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
+
+}
+
+showTime();
