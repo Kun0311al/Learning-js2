f9754b2f7d7dd378bd10ae38367b67d865d2f3ac
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-10-16 14:22:43 +0530

    final project

--- Date and time display/main.js
+++ Date and time display/main.js
@@ -1,30 +1,25 @@
-function showTime(){
-    var date = new Date()
-    var h = date.getHours()
-    var m = date.getMinutes()
-    var s = date.getSeconds()
-    var session = "AM"
-
-    if (h=0){
-        h=12;
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+  
+    if(hh == 0){
+        hh = 12;
     }
-
-    if(h > 12){
-        h = h - 12;
-        session = "PM"
-    }
-
-  h = h < 10 ? "0" + h : h;
-  m = m < 10 ? "0" + m : m;
-  s = s < 10 ? "0" + s : s;
-
-   var time = h + ":" + m + ":" + s + session
-
-  document.getElementById("MyClockDisplay").innerHTML = time;
-  document.getElementById("MyClockDisplay").textContent = time;
-
-  setTimeout(showTime, 1000);
-
-}
-
-showTime();
+    if(hh > 12){
+        hh = hh - 12;
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+      
+     let time = hh + ":" + mm + ":" + ss + " " + session;
+  
+    document.getElementById("MyClockDisplay").innerText = time; 
+    let t = setTimeout(()=> currentTime() , 1000);
+  }
+  currentTime();  
