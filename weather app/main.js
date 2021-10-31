fe6429e38fe45e7c3702802fbc9cdeb48ff70298
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-10-31 17:47:11 +0530

    getting data by js

--- weather app/main.js
+++ weather app/main.js
@@ -4,9 +4,18 @@ let weather = {
         fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey
         )
         .then((response) => response.json())
-        .then((data) => console.log(data));
+        .then((data) => this.displayWeather(data));
     },
     displayWeather: function(data) {
-
-    }
-}
+        const {name} = data;
+        const {icon ,description} = data.weather[0];
+        const {temp ,humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerHTML = `weather in ${name}`;
+        document.querySelector(".temp").innerHTML = `${temp}°C`
+        document.querySelector(".discription").innerHTML = description;
+        document.querySelector(".humidity").innerHTML = `Humidity : ${humidity}%`;
+        document.querySelector(".wind").innerHTML = `wind Speed : ${speed} km/h`;
+        }
+};
