4672c05d7cd438841bf365214191fa1031b4b13f
kunal parkar <83920024+Kun0311al@users.noreply.github.com>
2021-10-31 19:22:34 +0530

    some little changes

--- weather app/main.js
+++ weather app/main.js
@@ -1,21 +1,24 @@
 let weather = {
-    "apiKey": "23f16c74515bbcc07c5031e4acc310c1",
-    fetchWeather: function(city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey
-        )
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function(data) {
-        const {name} = data;
-        const {icon ,description} = data.weather[0];
-        const {temp ,humidity} = data.main;
-        const {speed} = data.wind;
-        console.log(name,icon,description,temp,humidity,speed);
-        document.querySelector(".city").innerHTML = `weather in ${name}`;
-        document.querySelector(".temp").innerHTML = `${temp}°C`
-        document.querySelector(".discription").innerHTML = description;
-        document.querySelector(".humidity").innerHTML = `Humidity : ${humidity}%`;
-        document.querySelector(".wind").innerHTML = `wind Speed : ${speed} km/h`;
-        }
-};
+        "apiKey": "23f16c74515bbcc07c5031e4acc310c1",
+        fetchWeather: function(city) {
+            fetch("https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=metric&appid="+ this.apiKey
+            )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+        },
+        displayWeather: function(data) {
+            const {name} = data;
+            const {icon ,description} = data.weather[0];
+            const {temp ,humidity} = data.main;
+            const {speed} = data.wind;
+            console.log(name,icon,description,temp,humidity,speed);
+            document.querySelector(".city").innerHTML = `weather in ${name}`;
+            document.querySelector(".temp").innerHTML = `${temp}°C`;
+            document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+            document.querySelector(".discription").innerHTML = description;
+            document.querySelector(".humidity").innerHTML = `Humidity : ${humidity}%`;
+            document.querySelector(".wind").innerHTML = `wind Speed : ${speed} km/h`;
+            }
+    };
+let input = document.getElementsByClassName("search-bar").value
+
